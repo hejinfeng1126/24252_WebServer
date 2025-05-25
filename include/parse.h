79bce170220de479e597fa5faa39eacf576a9b17
“hejinfeng” <2438582627@qq.com>
2025-05-25 15:54:30 +0800

    第三阶段完成，实现 HTTP 的并发请求

--- include/parse.h
+++ include/parse.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,24 +5,24 @@
 #define SUCCESS 0
 
 //Header field
-typedef struct {
-    char header_name[4096];
-    char header_value[4096];
+typedef struct
+{
+	char header_name[4096];
+	char header_value[4096];
 } Request_header;
 
 //HTTP Request Header
-typedef struct {
-    char http_version[50];
-    char http_method[50];
-    char http_uri[4096];
-    Request_header *headers;
-    int header_count;
+typedef struct
+{
+	char http_version[50];
+	char http_method[50];
+	char http_uri[4096];
+	Request_header *headers;
+	int header_count;
 } Request;
 
-Request *parse(const char *buffer, int size, int socketFd);
+Request* parse(char *buffer, int size,int socketFd);
 
 // functions decalred in parser.y
-extern int yyparse();
-
-
-extern void set_parsing_options(char *buf, size_t i, Request *request);
+int yyparse();
+void set_parsing_options(char *buf, size_t i, Request *request);
